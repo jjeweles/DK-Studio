3854a13b8612e253ad1b989bfeca223f4d0127e2
Justin Jewell <77253839+jjeweles@users.noreply.github.com>
2022-07-03 19:58:40 -0500

    add db for MMA entry

--- app.py
+++ app.py
@@ -11,15 +11,15 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dkstudio.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 os.environ["QTWEBENGINE_CHROMIUM_FLAGS"] = "--enable-logging --log-level=3"
+db = SQLAlchemy(app)
 
 
 class MMA(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fighter_name = db.Column(db.String(80))
-    fighter_class = db.Column(db.String(80))
     fighter_points = db.Column(db.Integer)
+    fighter_class = db.Column(db.String(80))
 
 
 class Nascar(db.Model):
@@ -32,6 +32,27 @@ class Nascar(db.Model):
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/mma_entry')
+def mma_entry():
+    return render_template('mma_entry.html')
+
+
+@app.route('/add_mma_entry', methods=['POST'])
+def add_mma_entry():
+    fighter_name = request.form['fighter_name']
+    fighter_points = request.form['fighter_points']
+    fighter_class = request.form['weight_class']
+    new_fighter = MMA(fighter_name=fighter_name, fighter_points=fighter_points, fighter_class=fighter_class)
+    db.session.add(new_fighter)
+    db.session.commit()
+    return redirect(url_for('mma_entry'))
+
+
+@app.route('/nascar_entry')
+def nascar_entry():
+    return render_template('nascar_entry.html')
 
 
 # Define function for QtWebEngine
@@ -50,4 +71,4 @@ if __name__ == "__main__":
     # start sub-thread to open the browser.
     Timer(1, lambda: ui("http://127.0.0.1:5000/")).start()
     db.create_all()
-    app.run(debug=False)
+    app.run(debug=True)
